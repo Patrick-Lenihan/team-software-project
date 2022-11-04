2d06ac2862e49a63a22f9b3dc8d5defce3daf950
Patrick-Lenihan <pdlenihan@gmail.com>
2022-11-04 16:47:51 +0000

    ben and patrick pair programing

---
+++ main.py
@@ -0,0 +1,51 @@
+class Main():
+	def __init__(self,distribution,producers):
+		self.distribution= distribution
+		self.producers = producers
+		self.prediction = Prediction()
+		self.market = Market()
+
+	def itterate(self):
+		time = 0
+		while True:
+			ussage = self.getUsage(time)
+			totalProduction = self.pollProducers()
+			
+			predictions = self.getPredictions(ussage)
+			winners,price = self.maket.getWinners()
+
+			self.sendOrders(winners)
+			'''
+			[
+				[
+					[producer,amount],
+					[producer,amount]
+				],[]]
+
+
+
+			{
+				producer: [amount,amount.....]
+			}
+			'''self.distribution.receivePrice(price)
+
+
+			time += 1
+
+	def getUsage(self,time):
+		return self.distribution.ussage(time)
+
+	def pollProducers(self):
+		total_energy = 0
+		for i in self.producers:
+			total_energy += i.currentProduction
+		return total_energy
+
+	def getPredictions(self,ussage):
+		return self.prediction.predict(ussage)
+	def sendOrders(self,winners):
+		for i in self.producers:
+			i.receiveOrder(winners[i])
+
+
+
