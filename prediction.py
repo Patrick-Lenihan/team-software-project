b9178b5624f57c525781c02fec51d609dc59e92a
cahillBen <98129211+cahillBen@users.noreply.github.com>
2022-11-16 22:40:07 +0000

    swapped order of sql statements to preserve data

--- prediction.py
+++ prediction.py
@@ -1,19 +1,13 @@
 import sqlite3 as sl
-from queue import Queue
 connect = sl.connect('app.db')
 cursor = connect.cursor()
 
 class Prediction():
     def __init__(self):
-        self.usage = 50 # last usage
+        self.usage = 0
         self.time = 0
         self.day = 0
-        self.history = Queue(maxsize=5)
-        self.history.put(0) # fill last 5 recorded usages with default values
-        self.history.put(0)
-        self.history.put(0)
-        self.history.put(0)
-        self.history.put(0)
+        self.history = [0,0,0,0,0]
     
     def checkTimeValue(self, time):
         if time == 24:
@@ -39,49 +33,57 @@ class Prediction():
         
         with connect:
             for i in range(5):
+                time = self.checkTimeValue(time)
                 cursor.execute('SELECT AVG(use) FROM use_history WHERE time = %f'%(time))
                 for value in cursor:
                     average = value
                 use_history.append(float(average[0]))
                 time += -.25
-                self.checkTimeValue(time)
+                
                 
             time = self.time
             for i in range(5):
+                time = self.checkTimeValue(time)
                 cursor.execute('SELECT AVG(use) FROM use_history WHERE time = %f'%(time))
                 for value in cursor:
                     average = value
                 historical_prediction.append(float(average[0]))
                 time += .25
-                self.checkTimeValue(time)
-        
-        return use_history.reverse(), historical_prediction
+                
+        use_history.reverse()
+        return use_history, historical_prediction
         
     def updateHistoricalData(self):
         with connect:
-            cursor.execute('UPDATE use_history SET use='+str(self.usage)+', time='+str(self.time)+', day='+str(self.day)+' WHERE time='+str(self.time)+' AND '+str(self.day))
+            cursor.execute('UPDATE use_history SET use='+str(self.usage)+' WHERE time='+str(self.time)+' AND day='+str(self.day))
         
     def predict(self, usage):
-        self.history.get()
-        self.history.put(self.usage) # remove oldest values and replace with new data
-        self.updateHistoricalData()
-        historical_data, historical_prediction = self.getHistoricalData()
-        
-        max_difference = 0 # biggest difference between a predicted value and its historical average
         self.usage = usage
+        self.history.pop(0)
+        self.history.append(self.usage) # remove oldest values and replace with new data
+        
+        historical_data, historical_prediction = self.getHistoricalData()
+        self.updateHistoricalData()
+
+        max_difference = 0 # biggest difference between a predicted value and its historical average
+        
+        print(self.history)
+        print(historical_prediction)
         for i in range(len(historical_data)):
-            if historical_data[i] < self.history[i] and (self.history - historical_data[i]) > maxDifference:
-                maxDifference = self.history - historical_data[i]
+            if historical_data[i] < self.history[i] and (self.history[i] - historical_data[i]) > max_difference:
+                max_difference = self.history[i] - historical_data[i]
+        
         for i in range(len(historical_prediction)):
             historical_prediction[i] += max_difference
+        print(historical_data)
         self.time += .25 # increment time for the next request  
         
-        self.time = self.checkTimeValue(self, self.time)
-        self.checkDayValue(self)
+        self.time = self.checkTimeValue(self.time)
+        self.checkDayValue()
         
         return historical_prediction
                 
 if __name__ == '__main__':
     prediction = Prediction()
-    print(prediction.predict(6220))  
+    print(prediction.predict(5000))  
                 
--- reset_db.py
+++ reset_db.py
@@ -17,3 +17,12 @@ with connect:
         for j in range(24): # hours
             for k in range(4):
                 connect.execute('''INSERT INTO use_history (use, time, day) VALUES (6220, %f, %d)'''%(j+k*.25,i))
+                
+    connect.execute('UPDATE use_history SET use=2000 WHERE time=0')
+    
+
+    cursor = connect.cursor()
+    cursor.execute('SELECT * FROM use_history WHERE time = 0')
+    for value in cursor:
+        print(value)
+        
