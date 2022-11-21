2b3daae17a2e6b97c5afd7456477a2be9baadd5a
James Kirkby <87252512+JamesKirkby-1@users.noreply.github.com>
2022-11-21 14:35:53 +0000

    Add files via upload
    Updated both classes

--- smartmeter.py
+++ smartmeter.py
@@ -3,8 +3,9 @@ The smart meter acts as the receiver of electricity from the substation.
 It calcualtes the amount of electricity used by the connected household and
 sends this data to the substation.
 '''
-
-import random
+import sqlite3 as sl
+connect = sl.connect('total.db')
+cursor = connect.cursor()
 
 class SmartMeter(object):
 
@@ -12,16 +13,24 @@ class SmartMeter(object):
     The Smart Meter class is used to calculate the amount of electricity used by a household
     '''
 
-    def __init__(self, usage):
+    def __init__(self):
 
         '''
         The initialiser for the meter class
 
         Args:
-            usage: the amount of electricity used at a given time by the connected household
+            received: the amount of electricity received from the substation
+            usage: the amount of electricity used by the connected household
+            time: the time at which the usage was calculated
+            num_meters: the number of smart meters 
+            ID: the ID of the row in the database
         '''
 
-        self._usage = usage
+        self._received = 0
+        self._usage = 0
+        self._time = 0
+        self._num_meters = 0
+        self._ID = 1
 
     def updateUsage(self):
 
@@ -32,16 +41,31 @@ class SmartMeter(object):
             usage: the amount of electricity used by the connected household
         '''
 
-        return self.calculateUsage(self._usage)
+        return self.calculateUsage()
 
     def calculateUsage(self):
 
         '''
-        Calculates the amount of electricity used by the connected households
-        (For now I will randomly generate a number between 3500 and 6000 - average daily usage)
+        Calculates the amount of electricity used by the connected households by 
+        reading the total consumption and number of smart meters at a given time
         '''
 
-        self._usage = random.randint(3500, 6000)
+        with connect:
+
+            query = f"SELECT time, usage, num_meters FROM total_usage WHERE ID = {self._ID}"
+
+            self._ID += 1
+
+            cursor.execute(query)
+
+            data = cursor.fetchall()
+            
+            for value in data:
+                self._usage = value[1]
+                return self._usage
+
+if __name__ == "__main__":
+    meter = SmartMeter()
 
 
         
