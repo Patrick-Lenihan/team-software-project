32b1d8313a5ddde9149311d862a9a7b065b86e9a
Patrick-Lenihan <pdlenihan@gmail.com>
2022-11-21 22:40:26 +0000

    update spelling of ussage to usage in simulation.py

--- simulation.py
+++ simulation.py
@@ -7,7 +7,7 @@ import powerstation
 """simulation is the simulation envirnonment the the main controler runs 
 in.
 
-it sets up databases, producers and distribution network and takes in ussage
+it sets up databases, producers and distribution network and takes in usage
 data from a csv. 
 
 """
@@ -18,50 +18,50 @@ class Simulation(object):
 	def __init__(self):
 		self._num_meters, self._main_substation = self.generateDistribution()
 		producers = self.generateProducers()
-		self.setUsages("ussage.csv")
+		self.setUsages("usage.csv")
 		self.setHistory()
 		contoler = main.Main(self._main_substation,producers)
 		contoler.Iterate()
-	def setUsages(self,ussage_csv):
+	def setUsages(self,usage_csv):
 		"""setUsages reads in the passes file path to 
 		a csv and creates the aplications database.
 
 		for each usage contained in the passed csv file 
-		an entery is entered into the ussage database.
+		an entery is entered into the usage database.
 		this will be used by the smart meter to return
-		its ussage to the main controller.
+		its usage to the main controller.
 		the time is incremented in .25 because the time
 		period of the csv is in 15min and this is a 
 		translation of mins to decimal. this is done to
 		be consistant with other tables in the aplication.
 
 		Args:
-			ussage_csv: the filepath to the csv containing
-						the ussage information.
+			usage_csv: the filepath to the csv containing
+						the usage information.
 		"""
 		conn, cursor = self.creatDatabase()
-		ussage_file= open(ussage_csv)
-		ussage_reader = csv.reader(ussage_file)
-		next(ussage_reader)
+		usage_file= open(usage_csv)
+		usage_reader = csv.reader(usage_file)
+		next(usage_reader)
 		rows = []
 		time = 0 
-		for row in ussage_reader:
+		for row in usage_reader:
 			self.addRow2DB(cursor,time,row)
 			time += 0.25
-		ussage_file.close()
+		usage_file.close()
 		conn.commit()
 	def creatDatabase(self):
 		conn = sqlite3.connect("app.db")
 		cursor = conn.cursor()
-		cursor.execute("DROP TABLE IF EXISTS ussage")
-		cursor.execute("CREATE TABLE ussage (decTime TEXT, ussage INTEGER)")
+		cursor.execute("DROP TABLE IF EXISTS usage")
+		cursor.execute("CREATE TABLE usage (decTime TEXT, usage INTEGER)")
 		return conn, cursor
 	def addRow2DB(self,cursor,time,row):
-		# we are deviding by the total ussage by the number of 
+		# we are deviding by the total usage by the number of 
 		# smart meteres here because want to get a realistic number.
 		# for each smart meter.
 		usage_per_meter = int(row[1])//self._num_meters
-		cursor.execute("INSERT INTO ussage VALUES (?, ?)",(time,usage_per_meter))
+		cursor.execute("INSERT INTO usage VALUES (?, ?)",(time,usage_per_meter))
 	def generateDistribution(self):
 		"""generates the distribution network.
 		creates a substation that the main controler 
@@ -83,11 +83,11 @@ class Simulation(object):
 		producers.append(powerstation.Producer(2.35,1,4000))
 		return producers
 	def setHistory(self):
-		"""sets the inital ussage history.
-		ussage history is importent for the prediction module.
+		"""sets the inital usage history.
+		usage history is importent for the prediction module.
 		this is to insure there is always usage history available even if it has not
 		been running before now.
-		it stores the ussage history in a database to be accessed by the prediction 
+		it stores the usage history in a database to be accessed by the prediction 
 		module.
 		"""
 		conn = sqlite3.connect('app.db')
